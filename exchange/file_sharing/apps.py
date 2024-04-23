b72bb9476fb1cccd4a0fec8a6ca1a9ebc75655bd
yellowcatdotcom <valentinagoncarenko975@gmail.com>
2024-04-23 18:31:38 +0300

    поломала и починила гит

--- exchange/file_sharing/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class FileSharingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'file_sharing'
-    verbose_name = 'Файлообменник'
